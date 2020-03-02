05849b13e90d0fe7d673096c3f81d03be114a077
tjdrnr0557 <tjdrnr05571@naver.com>
2020-03-03 00:31:09 +0900

    Remove All Dummy Data

--- front/pages/stores.js
+++ front/pages/stores.js
@@ -5,39 +5,6 @@ import { LOAD_STORES_REQUEST } from "../reducers/stores";
 
 import StoreCard from "../containers/StoreCard";
 
-const storesArr = [
-  {
-    id: 1,
-    name: "1. 필바든치킨",
-    kindId: 1,
-    description: "양념 순살이 가장 맛있는 필바든치킨",
-    img: "http://www.enewstoday.co.kr/news/photo/201604/511235_134361_648.jpg"
-  },
-  {
-    id: 1,
-    name: "2. 닭큐멘터리",
-    kindId: 1,
-    description: "양념 순살이 가장 맛있는 필바든치킨",
-    img:
-      "https://mblogthumb-phinf.pstatic.net/MjAxNzA1MDdfMTg3/MDAxNDk0MDgzOTk4NTU4.972mR0izakm1sqFgtBPMqXEW7rfpSttydtiQMm6lylgg.P_NxaO_-nhY_ZM9x23JQusjO8Su8uIoFPHqR8KgKiVYg.JPEG.mhj9722/IMG_5635.jpg?type=w800"
-  },
-  {
-    id: 1,
-    name: "3. 웨이사이드",
-    kindId: 1,
-    description: "양념 순살이 가장 맛있는 필바든치킨",
-    img:
-      "https://mp-seoul-image-production-s3.mangoplate.com/14039/533317_1474088500745_9216"
-  },
-  {
-    id: 1,
-    name: "4. 치킨하우스",
-    kindId: 1,
-    description: "양념 순살이 가장 맛있는 필바든치킨",
-    img: "http://www.enewstoday.co.kr/news/photo/201604/511235_134361_648.jpg"
-  }
-];
-
 const StoreWrapper = styled.div`
   width: 100%;
   min-height: 100%;
