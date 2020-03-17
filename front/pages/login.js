75e73e4129375a80ed481f06d846810d15337546
tjdrnr0557 <tjdrnr05571@naver.com>
2020-03-17 22:37:07 +0900

    Fix User Router in BackEnd Server

---
+++ front/pages/login.js
@@ -0,0 +1,9 @@
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import LoginForm from "../containers/LoginForm";
+
+const Login = () => {
+  return <LoginForm />;
+};
+
+export default Login;
